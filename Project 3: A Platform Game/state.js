aa3e81612ad372a14f6a80fb04a8704fec88f799
Joe Pickert <pickertjoe@gmail.com>
2021-12-04 16:12:53 -0600

    Kind of almost getting the linting/formatting right.....

--- Project 3: A Platform Game/state.js
+++ Project 3: A Platform Game/state.js
@@ -1,15 +1,15 @@
 class State {
   constructor(level, actors, status) {
-    Object.assign(this, { level, actors, status });
+    Object.assign(this, { level, actors, status })
   }
 
   static start(level) {
-    return new State(level, level.starActors, "playing");
+    return new State(level, level.starActors, 'playing')
   }
 
   get player() {
-    return this.actors.find((actor) => actor.type === "player");
+    return this.actors.find((actor) => actor.type === 'player')
   }
 }
 
-export default State;
+export default State
